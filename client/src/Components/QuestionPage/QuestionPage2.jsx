105ef54d7c1437b3481087a0f51e64d7916fdb63
Samson <94833173+Superior212@users.noreply.github.com>
2023-05-08 16:30:37 +0100

    registration page

---
+++ client/src/Components/QuestionPage/QuestionPage2.jsx
@@ -0,0 +1,40 @@
+import { GrCheckboxSelected, GrCheckbox } from "react-icons/gr";
+import oftenImage from "../../assets/img/often.png";
+
+const QuestionPage2 = () => {
+  return (
+    <div className="h-full flex flex-col justify-between sm:flex-row sm:h-auto">
+      <div className="sm:w-1/2">
+        <h1 className="font-bold font-open-sans text-4xl leading-14 text-[#0077B5] pt-20 pl-16">
+          Savvysaver
+        </h1>
+        <h2 className="font-semibold font-open-sans text-4xl leading-14 pt-16  pl-16">
+          How often do you earn?
+        </h2>
+        <div className="sm:pl-16 pl-16">
+          <p className="sm:pt-16 flex">
+            <GrCheckboxSelected className="mt-[0.5rem]" />
+            <span className="ml-4 text-lg sm:text-xl">Daily</span>{" "}
+          </p>
+          <p className="pt-4 flex">
+            <GrCheckbox className="mt-[0.5rem]" />
+            <span className="ml-4 text-lg sm:text-xl">weekly</span>{" "}
+          </p>
+          <p className="pt-4 flex">
+            <GrCheckboxSelected className="mt-[0.5rem]" />
+            <span className="ml-4 text-lg sm:text-xl">Monthly</span>{" "}
+          </p>
+        </div>
+        <button className="ml-16 mt-16 bg-[#0077B5] py-3 px-12 rounded-md text-white text-2xl">
+          Next
+        </button>
+      </div>
+
+      <div className="sm:w-1/2  text-4xl leading-14 pt-28 sm:pl-16  flex justify-center">
+        <img src={oftenImage} className="w-[300px] sm:w-[812px]" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default QuestionPage2;
